559132a7968678455109f43ffd7bbf3ad81cfe9c
Jedidiah <jedidiah.hwang.dev@gmail.com>
2022-08-11 19:59:38 -0600

    made basic repository layers using jparepository

---
+++ src/main/java/com/example/moodify/repositories/GenreRepository.java
@@ -0,0 +1,9 @@
+package com.example.moodify.repositories;
+
+import com.example.moodify.entities.Genre;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface GenreRepository extends JpaRepository<Genre, Long> {
+}

9e9cc3563a0b386c1a59bf887eda5fc1fbc2e99c
Jedidiah <jedidiah.hwang.dev@gmail.com>
2022-09-07 21:02:17 -0600

    cors config setup with maven packages and cors annotation

--- src/main/java/com/example/moodify/controllers/UserController.java
+++ src/main/java/com/example/moodify/controllers/UserController.java
@@ -4,13 +4,11 @@ import com.example.moodify.dtos.UserDto;
 import com.example.moodify.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(originPatterns = "http://localhost:8080")
 @RestController
 @RequestMapping("/api/v1/users")
 public class UserController {
